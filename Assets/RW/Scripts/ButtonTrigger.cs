ba09264a738a77ce950cb433a855a17bbac12552
manjogsingh <manjogsingh@gmail.com>
2019-01-04 21:03:30 +0530

    Inital working project

---
+++ Assets/RW/Scripts/ButtonTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour
+{
+    public Animator doorAnimator;
+    public GameObject message;
+    Animator buttonAnimator;
+    void Start ()
+    {
+        buttonAnimator = GetComponent<Animator> ();
+    }
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.tag.Equals ("Player"))
+        {
+            buttonAnimator.SetBool ("press", true);
+            doorAnimator.SetBool ("open", true);
+        }
+        if (transform.name.Equals ("Button 3"))
+        {
+            message.SetActive (true);
+        }
+    }
+}
