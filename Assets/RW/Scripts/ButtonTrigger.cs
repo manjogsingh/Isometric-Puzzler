5adf50e70734656c63e3ab97e010a954a61ba91b
manjogsingh <manjogsingh@gmail.com>
2019-01-06 02:57:39 +0530

    Bug Fixes - Fixed colliders - Fixed character size - Added Edge colliders

--- Assets/RW/Scripts/ButtonTrigger.cs
+++ Assets/RW/Scripts/ButtonTrigger.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 public class ButtonTrigger : MonoBehaviour
 {
-    public Animator doorAnimator;
-    public GameObject message;
-    Animator buttonAnimator;
-    void Start ()
-    {
-        buttonAnimator = GetComponent<Animator> ();
-    }
-    void OnTriggerEnter2D (Collider2D other)
-    {
-        if (other.tag.Equals ("Player"))
-        {
-            buttonAnimator.SetBool ("press", true);
-            doorAnimator.SetBool ("open", true);
-        }
-        if (transform.name.Equals ("Button 3"))
-        {
-            message.SetActive (true);
-        }
-    }
+	public GameObject door;
+	public GameObject message;
+	Animator buttonAnimator;
+	Animator doorAnimator;
+	Collider2D doorCollider;
+	void Start ()
+	{
+		buttonAnimator = GetComponent<Animator> ();
+		doorAnimator = door.GetComponent<Animator> ();
+		doorCollider = door.GetComponent<Collider2D> ();
+	}
+	void OnTriggerEnter2D (Collider2D other)
+	{
+
+		if (transform.name.Equals ("Button 3"))
+		{
+			message.SetActive (true);
+		}
+		if (other.tag.Equals ("Player"))
+		{
+			buttonAnimator.SetBool ("press", true);
+			doorAnimator.SetBool ("open", true);
+			doorCollider.enabled = false;
+		}
+	}
 }
